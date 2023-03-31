a55917cddb375dc8f8ae605c58923c919b1a7a80
Lohmarr <119974080+Lohmarr@users.noreply.github.com>
2023-03-30 22:31:08 -0400

    Added starter code files

--- index.js
+++ index.js
@@ -4,51 +4,78 @@ const inquirer = require('inquirer')
 const fs = require('fs')
 
 // Questions
-const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'Enter the title of your project:'
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Enter a description of your project:'
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'Enter installation instructions:'
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Enter usage information:'
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'Choose a license for the project:',
-        choices: ['MIT', 'GPL', 'Apache', 'BSD'],
-    },
-    {
-        type: 'input',
-        name: 'contributing',
-        message: 'Enter contribution guidelines:',
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: 'Enter test instructions:'
-    },
-    {
-        type: 'input',
-        name: 'username',
-        message: 'Enter your Github username:'
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Enter your email:'
-    },
-]
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Enter the title of your project:'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Enter a description of your project:'
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Enter installation instructions:'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Enter usage information:'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Choose a license for the project:',
+            choices: ['MIT', 'GPL', 'Apache', 'BSD'],
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Enter contribution guidelines:',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Enter test instructions:'
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: 'Enter your Github username:'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email:'
+        },
+    ])
+    .then((answers) => {
+        const readme = `
+        # ${answers.title}
+
+        ${answers.description}
+
+        ## Table of Contents
+
+        - [Installation](#installation)
+        - [Usage](#usage)
+        - [License](#license)
+        - [Contributing](#contributing)
+        - [Tests](#tests)
+        - [Questions](#questions)
+
+        ## Installation
+
+        ${answers.installation}
+
+        ## Usage
+
+        ${answers.usage}
+
+        ## License
+        `
+    })
